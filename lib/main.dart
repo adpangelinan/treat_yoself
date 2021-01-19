0208d857fbd994842236ecc56e97701739d6b2cc
Scynthius <31458602+Scynthius@users.noreply.github.com>
2021-01-19 16:53:45 -0500

    Fixed bug in main.

--- lib/main.dart
+++ lib/main.dart
@@ -60,37 +60,9 @@ class LandingPageLogged extends StatefulWidget {
 }
 
 class _LandingPageLoggedState extends State<LandingPageLogged> {
-  List<String> dbData;
-
-  @override
-  void initState() {
-    _setData();
-    super.initState();
-  }
-
-  void _setData() async {
-    dbData = await queryDB();
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: ListView.builder(
-            padding: EdgeInsets.symmetric(vertical: 12),
-            itemBuilder: (BuildContext ctxt, int index) {
-              return new Text(dbData[index]);
-            }));
+    return Container();
   }
 }
 
