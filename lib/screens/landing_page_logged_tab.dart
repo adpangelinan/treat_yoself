103ecd2f793bfc24f26fb4fe0debbde74ad7b993
adpangelinan <adrian.pang92@outlook.com>
2021-01-29 16:36:48 -0600

    Added shopping tiles to landing page, moved feed to it's own dart file

--- lib/screens/landing_page_logged_tab.dart
+++ lib/screens/landing_page_logged_tab.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'login_tab.dart';
+import 'list_tiles.dart';
+import 'feed.dart';
 
 /*Class Landing Paged Logged
 Landing Page for a user that is currently logged in
@@ -23,7 +25,7 @@ class _LandingPageLogged extends State<LandingPageLogged> {
         IconButton(
             icon: Icon(Icons.shopping_cart),
             onPressed: () {
-              Navigator.pushNamed(context, '/shoppinglist');
+              Navigator.pushNamed(context, '/shopping_list');
             }),
         IconButton(
             icon: Icon(Icons.camera_alt_outlined),
@@ -32,7 +34,7 @@ class _LandingPageLogged extends State<LandingPageLogged> {
             }),
       ]),
       drawer: _returnDrawer(),
-      body: Feed(),
+      body: _buildBody(),
       bottomNavigationBar: BottomAppBar(
           color: Colors.white,
           child: IconButton(icon: Icon(Icons.home), onPressed: _pushRoute)),
@@ -68,7 +70,7 @@ class _LandingPageLogged extends State<LandingPageLogged> {
             //leading: NewsTab.androidIcon,
             title: Text('Shopping List'),
             onTap: () {
-              Navigator.pushReplacementNamed(context, '/shoppinglist');
+              Navigator.pushReplacementNamed(context, '/shopping_list');
             },
           ),
           ListTile(
@@ -99,90 +101,15 @@ class _LandingPageLogged extends State<LandingPageLogged> {
   }
 }
 
-class Feed extends StatefulWidget {
-  @override
-  _FeedState createState() => _FeedState();
-}
+Widget _buildBody() {
+  //return TestingWidget();
 
-class _FeedState extends State<Feed> {
-  final _suggestions = [];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _buildTiles(),
-    );
-  }
-
-  Widget _buildTiles() {
-    return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _returnTiles(); /*4*/
-          }
-          return _returnTiles();
-        });
-  }
-
-  Widget _returnTiles() {
-    final leftSection = new Container(
-      child: new CircleAvatar(
-        backgroundImage: new NetworkImage('https://picsum.photos/250?image=9'),
-        backgroundColor: Colors.lightGreen,
-        radius: 24.0,
-      ),
-    );
-    final middleTile = new Expanded(
-        child: Container(
-            padding: EdgeInsets.only(left: 8.0),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  new Text(
-                    "Name",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  Text(
-                    "Vons is such a lit store!",
-                    style: TextStyle(color: Colors.grey),
-                  )
-                ])));
-
-    final rightSection = new Container(
-      child: new Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          new Text(
-            "9:50",
-            style: new TextStyle(color: Colors.lightGreen, fontSize: 12.0),
-          ),
-          new CircleAvatar(
-            backgroundColor: Colors.lightGreen,
-            radius: 10.0,
-            child: new Text(
-              "2",
-              style: new TextStyle(color: Colors.white, fontSize: 12.0),
-            ),
-          )
-        ],
-      ),
-    );
-
-    return new Container(
-      padding: new EdgeInsets.symmetric(vertical: 10.0, horizontal: 8.0),
-      height: 70.0,
-      child: new Row(
-        children: <Widget>[leftSection, middleTile, rightSection],
-      ),
-    );
-  }
+  return Container(
+      child: Column(children: [
+    Expanded(
+        child: Row(children: [
+      Container(width: 360, color: Colors.red, child: Feed())
+    ])),
+    Expanded(child: Row(children: [Container(width: 360, child: ListTiles())])),
+  ]));
 }
