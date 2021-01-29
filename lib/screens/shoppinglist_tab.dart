acbcb48c4ecd7a57bc60fa9e6dca499263c6cbef
adpangelinan <adrian.pang92@outlook.com>
2021-01-29 16:48:53 -0600

    changed shopping list route from "shoppinglist" to "shopping_list"

--- lib/screens/shoppinglist_tab.dart
+++ lib/screens/shoppinglist_tab.dart
@@ -1,134 +1,108 @@
 import 'package:flutter/material.dart';
 import '../widgets/drawer.dart';
 
-
 class ShoppingList extends StatefulWidget {
-  static String routeName = '/shoppinglist';
+  static String routeName = '/shopping_list';
   @override
   _ShoppingListState createState() => _ShoppingListState();
 }
 
 class _ShoppingListState extends State<ShoppingList> {
   @override
-  
-  Widget build(BuildContext context){  //build a news feed list 
+  Widget build(BuildContext context) {
+    //build a news feed list
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Treat Yo Self'),
-          actions: [IconButton(icon: Icon(Icons.shopping_cart), onPressed:  null)]),
-        drawer: SideDrawer()
-    ,
+      appBar: AppBar(title: Text('Treat Yo Self'), actions: [
+        IconButton(icon: Icon(Icons.shopping_cart), onPressed: null)
+      ]),
+      drawer: SideDrawer(),
       body: Lists(),
       bottomNavigationBar: BottomAppBar(
-        color: Colors.white ,
-        child: IconButton(icon: Icon(Icons.home), onPressed: _pushRoute)),
-        
+          color: Colors.white,
+          child: IconButton(icon: Icon(Icons.home), onPressed: _pushRoute)),
     );
   }
 
-  void _pushRoute(){
+  void _pushRoute() {
     Navigator.pushReplacementNamed(context, '/landing_page');
-    
   }
-  
-  }
+}
 
 class Lists extends StatefulWidget {
-
-
   @override
   _ListItemState createState() => _ListItemState();
-
-
 }
 
 class _ListItemState extends State<Lists> {
   List<Widget> _itemList = [];
 
-
-final topRow = new Expanded(
-        child: Container(
-            padding: EdgeInsets.only(left: 8.0),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  new Text(
-                    "Create New Shopping List",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16.0,
-                    ),
+  final topRow = new Expanded(
+      child: Container(
+          padding: EdgeInsets.only(left: 8.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                new Text(
+                  "Create New Shopping List",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16.0,
                   ),
+                ),
+              ])));
 
-                ])));
-
-
-_addNewRow(){
-return Row(
-        children:[ Container(
-            padding: EdgeInsets.only(left: 8.0),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  new Text(
-                    "Create New Shopping List",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16.0,
-                    ),
+  _addNewRow() {
+    return Row(children: [
+      Container(
+          padding: EdgeInsets.only(left: 8.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                new Text(
+                  "Create New Shopping List",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16.0,
                   ),
+                ),
+              ]))
+    ]);
+  }
 
-                ]))]);
-
-}
-
-@override
-Widget build(BuildContext context){
-  return Scaffold(
-    body: Column(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
       children: [
-        Row(
-          children: [  
+        Row(children: [
           topRow,
           IconButton(icon: Icon(Icons.add), onPressed: _addShoppingList)
-      ]
-    ),
-        
+        ]),
         Column(children: _itemList),
       ],
-      )
-    );
-    
+    ));
+  }
 
+  _addShoppingList() {
+    List<Widget> temp = _itemList;
+    temp.add(_addNewRow());
+    setState(() {
+      _itemList = temp;
+    });
+  }
 
-}
-
-
-
-_addShoppingList(){
-  List<Widget> temp = _itemList;
-  temp.add(_addNewRow());
-  setState(() {
-    _itemList = temp; 
-  });
-}
-
-_buildLists(){
-  return Container(
-    child: ListTile(
-    title: Text("Create List"),
+  _buildLists() {
+    return Container(
+        child: ListTile(
+      title: Text("Create List"),
       trailing: Icon(
         Icons.add,
-        color: Colors.blue,        
+        color: Colors.blue,
       ),
-      
-  ));
-
-
-}
-
+    ));
+  }
 }
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,65 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
- 
- class SideDrawer extends StatelessWidget{
+
+class SideDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      return Drawer(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.green),
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 20),
-                child: Icon(
-                  Icons.account_circle,
-                  color: Colors.green.shade800,
-                  size: 96,
-                ),
+    return Drawer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(color: Colors.green),
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 20),
+              child: Icon(
+                Icons.account_circle,
+                color: Colors.green.shade800,
+                size: 96,
               ),
             ),
-            ListTile(
-              //leading: SongsTab.androidIcon,
-              title: Text('Profile'),
-              onTap: () {
-                Navigator.pushReplacementNamed(context, '/');
-              },
-            ),
-            ListTile(
-              //leading: NewsTab.androidIcon,
-              title: Text('Shopping List'),
-              onTap: () {
-                Navigator.pushReplacementNamed(context, '/shoppinglist');
-              },
-            ),
-            ListTile(
-              //leading: ProfileTab.androidIcon,
-              title: Text('Find Items'),
-              onTap: () {
-                Navigator.pushReplacementNamed(context, '/item_search');
-              },
-            ),
-            ListTile(
-              //leading: SettingsTab.androidIcon,
-              title: Text("Shop Categories"),
-              onTap: () {
-                Navigator.pushReplacementNamed(context, '/category');
-              },
-            ),
-            // Long drawer contents are often segmented.
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-            ),
-            ListTile(
-              //leading: SettingsTab.androidIcon,
-              title: Text("Settings"),
-              onTap: () {
-                Navigator.pushReplacementNamed(context, '/user_settings');
-              },
-            ),
-          ],
-        ),
-      );
-    }
- }
+          ),
+          ListTile(
+            //leading: SongsTab.androidIcon,
+            title: Text('Profile'),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/');
+            },
+          ),
+          ListTile(
+            //leading: NewsTab.androidIcon,
+            title: Text('Shopping List'),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/shopping_list');
+            },
+          ),
+          ListTile(
+            //leading: ProfileTab.androidIcon,
+            title: Text('Find Items'),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/item_search');
+            },
+          ),
+          ListTile(
+            //leading: SettingsTab.androidIcon,
+            title: Text("Shop Categories"),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/category');
+            },
+          ),
+          // Long drawer contents are often segmented.
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+          ),
+          ListTile(
+            //leading: SettingsTab.androidIcon,
+            title: Text("Settings"),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, '/user_settings');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
